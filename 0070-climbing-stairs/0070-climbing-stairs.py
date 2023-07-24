f8febc96500166248997061b0591e557557a8b94
Jason Chen <jasonxchen888@gmail.com>
2023-07-24 16:01:15 -0400

    Time: 42 ms (78.93%), Space: 16.2 MB (93.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        s1, s2, ways = 1, 1,1
+        s1, s2, ways = 1, 1, 1
 
         for _ in range(n - 1):
             ways = s1 + s2
