732469f4dba2c123760273c96897d7230623b97e
Jason Chen <jasonxchen888@gmail.com>
2023-07-28 12:52:17 -0400

    Time: 48 ms (71.94%), Space: 16.3 MB (71.20%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,21 +1,5 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # nums = {}
-
-        # while n not in nums:
-        #     nums[n] = 1
-        #     total = 0
-        #     while n > 0:
-        #         digit = n % 10
-        #         total += digit * digit
-        #         n = (n - digit) / 10
-        #     n = total
-
-        #     if n == 1:
-        #         return True
-        
-        # return False
-
         slow = n
         fast = self.sumSquares(self.sumSquares(n))
 
@@ -24,7 +8,7 @@ class Solution:
             fast = self.sumSquares(self.sumSquares(fast))
             if fast == slow:
                 return False
-        
+
         return True
 
     def sumSquares(self, n: int) -> int:
