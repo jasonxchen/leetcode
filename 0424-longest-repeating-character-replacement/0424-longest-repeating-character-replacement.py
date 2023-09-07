ba2cec0d1e462ba731d51065c111610b9844633c
Jason Chen <jasonxchen888@gmail.com>
2023-09-06 23:27:17 -0400

    Time: 109 ms (61.89%), Space: 16.5 MB (40.06%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,23 +2,23 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l, r = 0, 0
         counts = {s[r]: 1}
-        max_length = 0
+        max_count = 1
+        max_count_curr = True
+        max_length = 1
 
         while r < len(s):
-            if r - l + 1 - counts[max(counts, key=counts.get)] - k <= 0:
+            if r - l + 1 - max_count - k <= 0:
                 max_length = max(max_length, r - l + 1)
                 r += 1
                 if r < len(s):
                     counts[s[r]] = counts.get(s[r], 0) + 1
+                    if counts[s[r]] > max_count:
+                        max_count = counts[s[r]]
+                        max_count_curr = True
             else:
+                if counts[s[l]] == max_count_curr:
+                    max_count_curr = False
                 counts[s[l]] -= 1
                 l += 1
-
-        # for l in range(len(s)):
-        #     counts = {s[l]: 1}
-        #     for r in range(l + 1, len(s)):
-        #         counts[s[r]] = counts.get(s[r], 0) + 1
-        #         if r - l + 1 - counts[max(counts, key=counts.get)] - k <= 0:
-        #             max_length = max(max_length, r - l + 1)
 
         return max_length
